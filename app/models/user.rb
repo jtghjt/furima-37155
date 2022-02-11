821b8f9d6314b4c1bef4195ef347c52759302d3e
jtghjt <junjunjun.t.13@gmail.com>
2022-02-12 02:33:15 +0900

    バリデーション(正規表現)の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'}
 
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
