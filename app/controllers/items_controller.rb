0c3e4fa8d0bb8d6f86b3c6b2fa6f1ae9beb7b1c8
jtghjt <junjunjun.t.13@gmail.com>
2022-02-15 01:45:31 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+
+  def item_params
+    params.requier(:item).permit(:image)
+
 end
