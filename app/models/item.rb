846b0d9b08051e663493e92320f46913afdbc185
jtghjt <junjunjun.t.13@gmail.com>
2022-02-15 12:03:52 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
+  # 各モデルのアソシエーション
   belongs_to :user
 
+  # ActiveStorageのアソシエーション
   has_one_attached :image
+
+  # ActivaHAshのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
 end
