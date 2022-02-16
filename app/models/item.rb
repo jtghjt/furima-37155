f0e8ad3fc7918a9bbab4a43203bb66885b81baa5
jtghjt <junjunjun.t.13@gmail.com>
2022-02-17 03:41:20 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   # ActiveStorageのアソシエーション
   has_one_attached :image
 
-  # ActivaHAshのアソシエーション
+  # ActivaHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
