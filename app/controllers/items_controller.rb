65e9824ce74799ecec0333b93a1ed605f3d04927
jtghjt <junjunjun.t.13@gmail.com>
2022-02-27 03:11:08 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,15 @@ class ItemsController < ApplicationController
     else
       redirect_to root_path
     end
+  end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item_params)
+    else
+      render :edit
+    end
   end
 
   private
