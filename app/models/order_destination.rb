e133aa0aa18200edbd14a51c2c7c7474037f71d5
jtghjt <junjunjun.t.13@gmail.com>
2022-03-08 02:39:02 +0900

    OrderDestinationクラス設定
    バリデーション設定
    データをテーブルに保存する処理

---
+++ app/models/order_destination.rb
@@ -0,0 +1,19 @@
+class OrderDestination
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :municipalities, :address, :building, :phone_number
+  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is Invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :municipalities
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/,message: "is invalid"}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(postcode: postcode, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
