cc23773736b8dbc083af75379378bbe304210832
jtghjt <junjunjun.t.13@gmail.com>
2022-03-08 03:21:14 +0900

    order_controllerの各アクション内の設定
    Formオブジェクトのインスタンス生成
    保存の可否での分岐設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,28 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_data_read, only: [:index, :create]
   
   def index
+    @order_destination = OrderDestination.new
   end
 
   def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def item_data_read
+    @item = Item.find(params[:item_id])
   end
 end
