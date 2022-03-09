4b45102407d742e3ecd4ed24816d5cf1929cd6b4
jtghjt <junjunjun.t.13@gmail.com>
2022-03-09 18:29:07 +0900

    orderコントローラーでのtoken情報のサーバーサイド処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_destination = OrderDestination.new(order_params)
     if @order_destination.valid?
+      pay_item
       @order_destination.save
       redirect_to root_path
     else
@@ -19,10 +20,19 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def item_data_read
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
