ba2d544c6a0587334bd3757964262ab29a93c551
jtghjt <junjunjun.t.13@gmail.com>
2022-03-10 01:54:34 +0900

    バリデーション修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -6,10 +6,10 @@ class OrderDestination
     validates :user_id
     validates :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is Invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :municipalities
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
     validates :token
   end
 
